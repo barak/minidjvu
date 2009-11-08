860f1af85b5e549ca48e97af3a5be05139676bd1
alih <alih@8ebc3b53-2358-4797-92d9-53f41ae555aa>
2009-11-08 17:28:21 +0000

    Sanitized headers in matcher/ a bit
    git-svn-id: https://minidjvu.svn.sourceforge.net/svnroot/minidjvu/trunk@16 8ebc3b53-2358-4797-92d9-53f41ae555aa

--- src/matcher/common.h
+++ src/matcher/common.h
@@ -1,27 +1,10 @@
-/* Plasma OCR - an OCR engine
- *
+/*
  * common.h - some common macros
- *
- * Copyright (C) 2006  Ilya Mezhirov
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 
-#ifndef PLASMA_OCR_COMMON_H
-#define PLASMA_OCR_COMMON_H
+#ifndef MDJVU_COMMON_H
+#define MDJVU_COMMON_H
 
 
 #ifdef HAVE_CONFIG_H
